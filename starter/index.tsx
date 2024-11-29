f47c5daf1f85731cb5e2ada49b7615e85942170e
uta-jeh4448 <123279355+jeh4448@users.noreply.github.com>
2024-11-29 19:16:09 +0000

    got a food place search to work in the basic example area

--- starter/index.tsx
+++ starter/index.tsx
@@ -1,36 +1,10 @@
-/**
- * Copyright 2024 Google LLC
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *    https://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
-
-
-import React from 'react';
-import {createRoot} from "react-dom/client";
-//import {APIProvider} from '@vis.gl/react-google-maps';
-import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
-
 let map: google.maps.Map;
-
-
-
-
 
 async function initMap() {
     const { Map, InfoWindow } = await google.maps.importLibrary('maps') as google.maps.MapsLibrary;
 
     let center = new google.maps.LatLng(52.369358, 4.889258);
-    console.log("map should be set here")
+
     map = new Map(document.getElementById('map') as HTMLElement, {
         center: center,
         zoom: 11,
@@ -91,21 +65,3 @@ async function nearbySearch() {
 }
 
 initMap();
-
-const App = () => (
-  <APIProvider apiKey={'AIzaSyDvsTyOT5uX43q0nWmtD9c09GFv7aKNU4k'} onLoad={() => console.log('Maps API has loaded.')}>
-     {/*<Map
-       defaultZoom={13}
-       defaultCenter={ { lat: 33.860664, lng: -100.208138 } }
-       onCameraChanged={ (ev: MapCameraChangedEvent) =>
-         console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-       }>
- </Map>*/}
-  </APIProvider>
- );
- 
- const root = createRoot(document.getElementById('app'));
- root.render(<App />);
- 
-
-export default App;

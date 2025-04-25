22d71ea4a82ce11762f8414d6ac10caa05ae0060
Diauu <113753280+Diauu@users.noreply.github.com>
2025-04-24 22:03:24 -0500

    added inbox files

--- App.jsx
+++ App.jsx
@@ -21,6 +21,7 @@ import Terms from './src/screen/Terms.jsx';/// TERMS CONDITONSSSSS JUST UPDATED
 import Logout from './src/screen/Logout'; // LOGOUT UPDATTTTTEED
 import About from './src/screen/About'; // LOGOUT UPDATTTTTEED
 import ContactSupport from './src/screen/ContactSupport'; // CONTACT SUPPORT UPDATED
+import NewGroupScreen from "./src/screen/NewGroupScreen.jsx";
 
 
 const Stack = createNativeStackNavigator();
@@ -43,15 +44,17 @@ const App = () => {
                 <Stack.Screen name="Inbox" component={Inbox} />
                 <Stack.Screen name="Chat" component={ChatScreen} />
                 <Stack.Screen name="NewMessage" component={NewMessageScreen} />
-                <Stack.Screen name="Matches" component={Matches}/>
-                <Stack.Screen name="MatchSetup" component={MatchSetup}/>
+                <Stack.Screen name="Matches" component={Matches} />
+                <Stack.Screen name="MatchSetup" component={MatchSetup} />
                 <Stack.Screen name="WebViewScreen" component={WebViewScreen} />
-                <Stack.Screen name="MenuButton" component={MenuButton}/>
-                <Stack.Screen name="PreferencesScreen" component={PreferencesScreen}/>
-                <Stack.Screen name="Terms" component={Terms} /> 
+                <Stack.Screen name="MenuButton" component={MenuButton} />
+                <Stack.Screen name="PreferencesScreen" component={PreferencesScreen} />
+                <Stack.Screen name="Terms" component={Terms} />
                 <Stack.Screen name="Logout" component={Logout} />
-                <Stack.Screen name="About" component={About}/>
-                <Stack.Screen name="ContactSupport" component={ ContactSupport}/>
+                <Stack.Screen name="About" component={About} />
+                <Stack.Screen name="ContactSupport" component={ContactSupport} />
+                <Stack.Screen name="NewGroup" component={NewGroupScreen} />
+
 
             </Stack.Navigator>
         </NavigationContainer>
